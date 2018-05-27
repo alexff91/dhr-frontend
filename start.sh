88d1ed1fab5fe06c65f4d209177d56eb99372790
Aleksandr.Fedorov <aleksandr.fedorov@billing.ru>
2018-05-27 18:24:50 +0300

    port

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 docker build -t front .
 docker stop front
 docker rm front
-docker run -p 80:8080 --privileged=true --restart=always --name=front -d front
+docker run -p 443:8080 --privileged=true --restart=always --name=front -d front
