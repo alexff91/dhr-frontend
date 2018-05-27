dd43c1e561bd08fe396182d2af1263041cdff107
Aleksandr.Fedorov <aleksandr.fedorov@billing.ru>
2018-05-27 19:20:30 +0300

    fix

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 docker build -t front .
+docker stop proxy
 docker stop front
+docker rm proxy
 docker rm front
 docker run -p 5443:8080 --privileged=true --restart=always --name=front -d front
-docker run -p 80:80 -p 443:443 -d --link="front" --restart=always -e "DOMAINS:'videointerview.online -> http://localhost:5443'" steveltn/https-portal:1
+docker run -p 80:80 -p 443:443 -d --link="front" --name=proxy --restart=always -e "DOMAINS:'videointerview.online -> http://localhost:5443'" steveltn/https-portal:1
