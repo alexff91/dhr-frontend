f0f79151b3b74161ca526bdb1a8f20545cb9d7cb
Aleksandr.Fedorov <aleksandr.fedorov@billing.ru>
2018-05-27 19:08:44 +0300

    proxy server

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 docker build -t front .
 docker stop front
 docker rm front
-docker run -p 443:8080 --privileged=true --restart=always --name=front -d front
+docker run -p 5443:8080 --privileged=true --restart=always --name=front -d front
+docker run -p 80:80 -p 443:443 -d --link="front"--restart=always -e "DOMAINS:'videointerview.online'" steveltn/https-portal:1
