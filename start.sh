8013b44c4db3e68dd96d0e3a525936c915b91e34
Aleksandr.Fedorov <aleksandr.fedorov@billing.ru>
2018-05-28 19:24:14 +0300

    build front and serve with https portal

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ docker stop front
 docker rm proxy
 docker rm front
 rm -rf /data/https-portal/vhosts/vi-hr.com/dist
-docker run --name=front -v /data/https-portal/vhosts/vi-hr.com:/dist front
+docker run --name=front -v /data/https-portal/vhosts/vi-hr.com:/usr/src/app/dist front
 docker run -p 443:443 -p 80:80 -e DOMAINS='vi-hr.com' -v /data/https-portal/vhosts:/var/www/vhosts --net=host --privileged=true --restart=always --name=proxy -d steveltn/https-portal
