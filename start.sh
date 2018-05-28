416662f3a67d918894b2fff7e992f99e6bf7d038
Aleksandr.Fedorov <aleksandr.fedorov@billing.ru>
2018-05-28 20:25:07 +0300

    fix

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ docker stop proxy
 docker stop front
 docker rm proxy
 docker rm front
-rm -rf /data/https-portal/vhosts/vi-hr.com/dist
-docker run --name=front --privileged=true -v /tmp/vi-hr.com:/usr/src/app/dist front
-cp -R /tmp/vi-hr.com/* /data/https-portal/vhosts/
-docker run -p 443:443 -p 80:80 -e DOMAINS='vi-hr.com' -v /data/https-portal/vhosts:/var/www/vhosts --net=host --privileged=true --restart=always --name=proxy -d steveltn/https-portal
+rm -rf /data/https-portal/vhosts/vi-hr.com/
+docker run --name=front --privileged=true  front
+docker cp front://usr/src/app/dist/* . /data/https-portal/vhosts/vi-hr.com/
+docker run -p 443:443 -p 80:80 -e DOMAINS='vi-hr.com' -v /data/https-portal/vhosts/:/var/www/vhosts --net=host --privileged=true --restart=always --name=proxy -d steveltn/https-portal
