ea02e821bf5d6cdc60adf25b0e9b3fe73402dcaf
Aleksandr.Fedorov <aleksandr.fedorov@billing.ru>
2018-05-28 23:16:39 +0300

    add https to backend url + fix start script

--- start.sh
+++ start.sh
@@ -1,11 +1,6 @@
 docker build -t front .
-docker stop proxy
 docker stop front
-docker rm proxy
 docker rm front
-rm -rf /data/https-portal/vhosts/vi-hr.com/
+rm -rf /var/www/fedorov.one/*
 docker run --name=front --privileged=true  front
-docker cp front:/usr/src/app/dist /tmp/vi-hr.com/
-rm -rf /data/https-portal/vhosts/vi-hr.com/*
-cp -Rf /tmp/vi-hr.com/dist/* /data/https-portal/vhosts/vi-hr.com/
-docker run -p 443:443 -p 80:80 -e DOMAINS='vi-hr.com' -v /data/https-portal/vhosts/:/var/www/vhosts --privileged=true --restart=always --name=proxy -d steveltn/https-portal
+docker cp front:/usr/src/app/dist /var/www/fedorov.one/
