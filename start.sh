d8b7354ad30d3b307809581c1f5c58f44682d9e1
Aleksandr.Fedorov <aleksandr.fedorov@billing.ru>
2018-05-28 20:37:53 +0300

    fix

--- start.sh
+++ start.sh
@@ -6,5 +6,6 @@ docker rm front
 rm -rf /data/https-portal/vhosts/vi-hr.com/
 docker run --name=front --privileged=true  front
 docker cp front:/usr/src/app/dist /tmp/vi-hr.com/
-cp -R /tmp/vi-hr.com/* /data/https-portal/vhosts/vi-hr.com/
+rm -rf /data/https-portal/vhosts/vi-hr.com/*
+cp -Rf /tmp/vi-hr.com/dist/* /data/https-portal/vhosts/vi-hr.com/
 docker run -p 443:443 -p 80:80 -e DOMAINS='vi-hr.com' -v /data/https-portal/vhosts/:/var/www/vhosts --net=host --privileged=true --restart=always --name=proxy -d steveltn/https-portal
