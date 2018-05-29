715dd1fb528984ae87320cb0772d03b6df97e480
Aleksandr Fedorov <alexff91@gmail.com>
2018-05-29 17:57:31 +0000

    start.sh edited online with Bitbucket

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 docker build -t front .
 docker stop front
 docker rm front
-rm -rf /var/www/fedorov.one/*
+rm -rf /var/www/vi-hr.com/demo/*
 docker run --name=front --privileged=true  front
-docker cp front:/usr/src/app/dist /var/www/fedorov.one/
+docker cp front:/usr/src/app/dist/* /var/www/vi-hr.com/demo/
