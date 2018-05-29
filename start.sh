8d6e6d79c4f5c052990344345d3d5aae920a55df
Aleksandr Fedorov <alexff91@gmail.com>
2018-05-29 18:05:16 +0000

    start.sh edited online with Bitbucket

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 docker build -t front .
 docker stop front
 docker rm front
-rm -rf /var/www/vi-hr.com/demo/*
+rm -rf /tmp/dist/*
 docker run --name=front --privileged=true  front
-docker cp front:/usr/src/app/dist/* /var/www/vi-hr.com/demo/
+docker cp front:/usr/src/app/dist /tmp 
+mkdir -p /var/www/vi-hr.com/demo
+cp -Rf /tmp/dist/* /var/www/vi-hr.com/demo/
